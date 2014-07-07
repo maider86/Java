1794e80b1ccf545774f677d3f30aac99c407efcc
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-07 15:19:21 +0200

    Ejemplo de variables y operadores

---
+++ Variables/src/Variables.java
@@ -0,0 +1,21 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int integer = 5;
+		String message;
+		
+		int num1 = 5;
+		int num2 = 10;
+		
+		message = "Hola";
+		System.out.println(integer+" "+message);
+		
+		System.out.println("La Suma entre "+num1+" y "+num2+" es igual a "+(num1+num2));
+		System.out.println("La Resta entre "+num1+" y "+num2+" es igual a "+(num1-num2));
+		System.out.println("La Multiplicación entre "+num1+" y "+num2+" es igual a "+(num1*num2));
+		System.out.println("La División entre "+num1+" y "+num2+" es igual a "+(num1/num2));
+		
+	}
+
+}

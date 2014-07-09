42e1c516a12697a189478bf571a28f062555f8e3
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-09 15:53:15 +0200

    Añadido el ejemplo de KeyListener

--- GUI_GridBagLayout/src/VentanaChat.java
+++ GUI_GridBagLayout/src/VentanaChat.java
@@ -2,6 +2,8 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -9,10 +11,10 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class VentanaChat extends JFrame implements ActionListener{
-	
+public class VentanaChat extends JFrame implements ActionListener, KeyListener{
+
 	private static final long serialVersionUID = 3252232561322747128L;
-	
+
 	private JTextArea area;
 	private JScrollPane scroll;
 	private JTextField texto;
@@ -29,12 +31,12 @@ public class VentanaChat extends JFrame implements ActionListener{
 
 	public void definirVentana(){
 		GridBagConstraints gbc = new GridBagConstraints();
-		
+
 		area = new JTextArea();
 		scroll = new JScrollPane(area); //Como parámetro se le pasa el componente al que le afectará la barra de desplazamiento
 		texto = new JTextField(20);
 		boton = new JButton("Enviar");
-		
+
 		//Definición de las restricciones para el TextArea y el Scroll
 		area.setEditable(false);
 		gbc.gridx = 0; //Columna en la que empezará
@@ -45,7 +47,7 @@ public class VentanaChat extends JFrame implements ActionListener{
 		gbc.weighty = 1.0;
 		gbc.fill = GridBagConstraints.BOTH; //Que crezca a ambos lados
 		add(scroll,gbc);
-		
+
 		//Definición de las restricciones para el TextField 
 		gbc.gridx = 0;
 		gbc.gridy = 1;
@@ -55,7 +57,7 @@ public class VentanaChat extends JFrame implements ActionListener{
 		gbc.weighty = 0.0; //Para que mantenga su ancho original
 		gbc.fill = GridBagConstraints.HORIZONTAL;
 		add(texto,gbc);
-		
+
 		//Definición de las restricciones para el Button
 		gbc.gridx = 1;
 		gbc.gridy = 1;
@@ -65,14 +67,33 @@ public class VentanaChat extends JFrame implements ActionListener{
 		gbc.weighty = 0.0;
 		gbc.fill = GridBagConstraints.NONE; //Para que no crezca
 		add(boton,gbc);
-		
+
 		boton.addActionListener(this);
+		texto.addKeyListener(this);
 	}
-	
+
 	public void actionPerformed(ActionEvent e) {
 		if (e.getSource() == boton){
 			area.append(texto.getText()+"\n");
 			texto.setText(""); //Vaciar el TextField
 		}
+	}
+
+	public void keyTyped(KeyEvent e) { //El tiempo que se mantiene pulsada la tecla
+
+	}
+
+	public void keyPressed(KeyEvent e) { //El momento en el que se pulsa la tecla
+		//El componente que debe escuchar el evento de la tecla es el textField
+		switch(e.getKeyCode()){ //Obtiene el número de la tecla que se haya pulsado
+			case KeyEvent.VK_ENTER:
+				area.append(texto.getText()+"\n");
+				texto.setText(""); //Vaciar el TextField
+				break;
+		}
+	}
+
+	public void keyReleased(KeyEvent e) { //El momento en el que se suelta la tecla
+
 	}
 }

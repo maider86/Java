ff025c29d507fa133fd84f4a97fecac85cf5272a
Maider de Jesus <maiderdejesus@gmail.com>
2014-07-17 12:45:10 +0200

    Ejemplo de JDBC

---
+++ JDBC/src/BDManagement.java
@@ -0,0 +1,40 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.xml.ws.Response;
+
+
+public class BDManagement {
+
+	private final static String driver = "com.mysql.jdbc.Driver";
+	private final static String db = "jdbc:mysql://localhost:3306/nikojavatutorialschema";
+	private final static String user = "root";
+	private final static String password = "passroot";
+	
+	private Connection connection;
+	private Statement st;
+	
+	public BDManagement(){
+		try{
+			Class.forName(driver);
+			connection = DriverManager.getConnection(db, user, password);
+			st = connection.createStatement();
+			System.out.println("Conexión con la base de datos");
+		}catch(Exception e){
+			System.out.println("No se pudo conectar a la base de datos");
+		}
+	}
+	
+	public void showAll(){
+		try{
+			ResultSet rs = st.executeQuery("SELECT * FROM personas");
+			while (rs.next()){
+				System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3));
+			}
+		}catch (Exception e){
+			System.out.println("Error al realizar la consulta");
+		}
+	}
+}
---
+++ JDBC/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		BDManagement bdm = new BDManagement();
+		bdm.showAll();
+	}
+
+}
